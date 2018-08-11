83023358e54118637c48357a5f45725bb4615c1c
yannis Su <82993170@qq.com>
2018-08-11 07:36:12 +0000

    完成用户删改查

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -8,6 +8,22 @@ use App\Models\User;
 
 class UsersController extends Controller
 {
+	public function __construct()
+    {
+        $this->middleware('auth', [            
+            'except' => ['show', 'create', 'store','index'] //通过 except 方法来设定 指定动作 不使用 Auth 中间件进行过滤
+        ]);
+        $this->middleware('guest', [
+            'only' => ['create']
+        ]);
+    }
+
+    public function index()
+    {
+        $users = User::paginate(10);
+        return view('users.index', compact('users'));
+    }
+
     //注册页
     public function create()
     {
@@ -39,5 +55,40 @@ class UsersController extends Controller
 
     	session()->flash('success','欢迎，您将在这里开启一段新的旅途~');
     	return redirect()->route('users.show',[$user]);
+    }
+
+    public function edit(User $user)
+    {
+		$this->authorize('update', $user);
+        return view('users.edit', compact('user'));
+    }
+
+    public function update(User $user, Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'password' => 'nullable|confirmed|min:6'
+        ]);
+
+        $this->authorize('update', $user);
+
+        $data = [];
+        $data['name'] = $request->name;
+        if ($request->password) {
+            $data['password'] = bcrypt($request->password);
+        }
+        $user->update($data);
+
+        session()->flash('success', '个人资料更新成功！');
+
+        return redirect()->route('users.show', $user->id);
+    }
+
+    public function destroy(User $user)
+    {
+    	$this->authorize('destroy', $user);
+        $user->delete();
+        session()->flash('success', '成功删除用户！');
+        return back();
     }
 }
